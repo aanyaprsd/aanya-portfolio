480be970b77e4a90e14de16c60faa110c35b0ce5
aanya prasad <aanya@aanyas-Air.localdomain>
2026-07-31 11:50:43 -0500

    footer update, contact page fleshed out, idk what else

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ const currentYearElement = document.getElementById("current-year");
 if (currentYearElement) {
     currentYearElement.textContent = new Date().getFullYear();
 }
+
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    alert("Message sending isn't connected yet!");
+});
